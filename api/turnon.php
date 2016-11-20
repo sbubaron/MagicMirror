e5d05f1fa89d98feb15cd440287dd174855ff314
Richard von Rauchhaupt <Richard.vonRauchhaupt@stonybrook.edu>
2016-11-20 08:36:49 -0500

    php command test

--- api/turnon.php
+++ api/turnon.php
@@ -4,7 +4,7 @@
 //$old_path = getcwd();
 
 $cmd = shell_exec('tvservice -o');
-
+echo '<pre>' . $cmd . '</pre>';
 //chdir('/my/path/');
 //$output = shell_exec('./script.sh var1 var2');
 //chdir($old_path);
